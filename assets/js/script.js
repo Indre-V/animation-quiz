fa9c6e4ab7ba700ddd71c0beb7d6d4dfc2b78022
Indre Vilickaite <vilickaite@yahoo.ie>
2024-01-16 20:56:48 +0000

    fix: favicon icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,46 @@
 const startQuizArea = document.querySelector("#start-area");
-const questionArea = document.querySelector ("#question-area");
+const questionArea = document.querySelector("#question-area");
 
-
-const hideArea = area => area.classList.add("hide");// function to hide
-const displayArea = area => area.classList.remove("hide");// function to display area 
-
-
+const hideArea = (area) => area.classList.add("hide"); // function to hide
+const displayArea = (area) => area.classList.remove("hide"); // function to display area
 
 //function to activate level buttons
-const activateButton = selectedLevelBtn => {
-    document.querySelectorAll('.level-btns').forEach(button => button.classList.remove('active'));
-    selectedLevelBtn.classList.add('active');
+const activateButton = (selectedLevelBtn) => {
+  document
+    .querySelectorAll(".level-btns")
+    .forEach((button) => button.classList.remove("active"));
+  selectedLevelBtn.classList.add("active");
 };
 
 // Wait for document to load
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const start = document.getElementById("start");
+  const usernameEntered = document.getElementById("username");
 
-    const start = document.getElementById('start');
-     const usernameEntered = document.getElementById('username');
+  // add event listener for Start Quiz button when Username and Level Selected
+  start.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    // add event listener for Start Quiz button when Username and Level Selected 
-    start.addEventListener('submit', function (event) {
-        event.preventDefault();
+    const selectedLevel = document.querySelector(".level-btns.active");
 
-        const selectedLevel = document.querySelector('.level-btns.active');
+    if (usernameEntered.value && selectedLevel) {
+      // Hide start area and display question area functions
+      hideArea(startQuizArea);
+      displayArea(questionArea);
 
-        if (usernameEntered.value && selectedLevel) {
-            // Hide start area and display question area functions
-            hideArea(startQuizArea);
-            displayArea(questionArea);
+      //add function to load quiz questions based on the level selected
+    } else {
+      alert("Please select Username and Level.");
+    }
+  });
 
-            //add function to load quiz questions based on the level selected
+  // Add event listener to level buttons
 
-        } else {
-            alert('Please select Username and Level.');
-        }
+  document.querySelectorAll(".level-btns").forEach(function (button) {
+    button.addEventListener("click", function () {
+      activateButton(button);
     });
-
-     // Add event listener to level buttons
-
-     document.querySelectorAll('.level-btns').forEach(function (button) {
-        button.addEventListener('click', function () {
-            activateButton(button);
-         });
-     });
-
-    
+  });
 });
 
 /**
@@ -54,11 +49,11 @@ document.addEventListener('DOMContentLoaded', function () {
  */
 
 function showInstructions(show) {
-    let instructionsContainer = document.getElementById('instructions');
-    instructionsContainer.style.display = show ? 'block' : 'none';
+  let instructionsContainer = document.getElementById("instructions");
+  instructionsContainer.style.display = show ? "block" : "none";
 }
 
 function showForm(show) {
-    let feedbackContainer = document.getElementById('feedback');
-    feedbackContainer.style.display = show ? 'block' : 'none';
+  let feedbackContainer = document.getElementById("feedback");
+  feedbackContainer.style.display = show ? "block" : "none";
 }
