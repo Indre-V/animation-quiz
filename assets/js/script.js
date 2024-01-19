285ba271f52597a74f6209f4fc301ada0a218df7
Indre Vilickaite <vilickaite@yahoo.ie>
2024-01-19 11:38:19 +0000

    feat: correct score counter to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,49 @@
-const startQuizArea = document.querySelector('#start-area');
-const questionArea = document.querySelector('#question-area');
-const gameOverArea = document.querySelector('#game-over');
-const nextButton = document.getElementById('next-btn');
-const progressText = document.getElementById('progress-text');
-const progressBarFull = document.getElementById('bar-full');
+//Elements of the code where adapted from https://github.com/jamesqquick/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript/blob/master/6.%20Create%20a%20Progress%20Bar/game.js
 
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-const answerButtons = document.querySelectorAll('.btn-a');
-const answerButtonsArray = Array.from(document.getElementsByClassName('btn-a'));
+const startQuizArea = document.querySelector("#start-area");
+const questionArea = document.querySelector("#question-area");
+const gameOverArea = document.querySelector("#game-over");
+const nextButton = document.getElementById("next-btn");
+const progressText = document.getElementById("progress-text");
+const progressBarFull = document.getElementById("bar-full");
 
-const timeCount = document.getElementById('seconds');
+const scoreText = document.getElementById("score");
+const timeCount = document.getElementById("seconds");
 
-const hideArea = (area) => area.classList.add('hide'); // function to hide
-const displayArea = (area) => area.classList.remove('hide'); // function to display area
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const answerButtons = document.querySelectorAll(".btn-a");
+const answerButtonsArray = Array.from(document.getElementsByClassName("btn-a"));
+
+const hideArea = (area) => area.classList.add("hide"); // function to hide
+const displayArea = (area) => area.classList.remove("hide"); // function to display area
 
 let currentQuestion = {};
-let userScore = 0;
+let score = 0;
 let questionNumber = 0;
 
-let selectedLevel = '';
+let selectedLevel = "";
 let questions = []; // Add questions array
-
+let acceptingAnswers = true;
 let timeCounter;
 
+//CONSTANTS
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 10;
 
 /**
- * Timer on the quiz
+ * This function increments the correct score.
+ *
+ */
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+/**
+ * Timer for the quiz the quiz
+ *
  */
 
 function startTimer(time) {
@@ -34,7 +51,7 @@ function startTimer(time) {
 
   function updateDisplay() {
     const seconds = remainingTime % 60;
-    const formattedTime = seconds.toString().padStart(2, '0');
+    const formattedTime = seconds.toString().padStart(2, "0");
     timeCount.textContent = formattedTime;
   }
 
@@ -50,37 +67,60 @@ function startTimer(time) {
   }, 1000);
 }
 
-function gameOver() {
+// Gave over function that display Final score
 
+function gameOver() {
   displayArea(gameOverArea);
   hideArea(questionArea);
+  displayFinalScore();
 
+  // Log a message (optional)
+  console.log("Game over");
 }
 
-  /***
-     * Function to restart the quiz
-     */
+//function to display final score
+//Include score message
 
-  document.getElementById('play-again-btn').addEventListener('click', resetGame);
+function displayFinalScore() {
+  const usernameEntered = document.getElementById("username").value;
+  const score = parseInt(document.getElementById("score").textContent);
 
-function resetGame(){
-  
-  //refresh page to Start Area
+  let scoreMessage = "";
+
+  if (score < 5) {
+    scoreMessage = `${usernameEntered} can do better! Keep trying.`;
+  } else {
+    scoreMessage = `Congratulations! ${usernameEntered} done a great job!`;
+  }
+
+  document.getElementById("score-message").textContent = scoreMessage;
+
+  document.querySelector(".correct-score").textContent = score;
+}
+
+// Function to restart the game
+
+document.getElementById("play-again-btn").addEventListener("click", resetGame);
+
+function resetGame() {
   location.reload();
-
 }
 
 // function to shuffle answers
-const shuffle = (answers) => answers.sort(() => Math.random() - 0.5)
+const shuffle = (answers) => answers.sort(() => Math.random() - 0.5);
 
 // fetch API to Load questions
-fetch('https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple')
-  .then(results => results.json())
-  .then(loadedQuestions => {
-    questions = loadedQuestions.results.map(apiQuestions => formatQuestions(apiQuestions));
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple"
+)
+  .then((results) => results.json())
+  .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((apiQuestions) =>
+      formatQuestions(apiQuestions)
+    );
     startGame();
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
 
@@ -90,114 +130,117 @@ const formatQuestions = (apiQuestions) => {
     difficulty: apiQuestions.difficulty,
     question: apiQuestions.question,
     correctAnswer: apiQuestions.correct_answer,
-    answers: shuffle([...apiQuestions.incorrect_answers, apiQuestions.correct_answer])
+    answers: shuffle([
+      ...apiQuestions.incorrect_answers,
+      apiQuestions.correct_answer,
+    ]),
   };
 };
 
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 10;
+// function to start the game when Start Quiz clicked
 
 startGame = () => {
   questionNumber = 0;
-  userScore = 0;
+  score = 0;
   getNewQuestion();
   startTimer(20);
-
-
 };
+
+/**get new question function to load a new question onto the quiz
+ * Enable answer buttons for the new question
+ * Display question text
+ * Assign each answer to a specific button ***/
 
 const getNewQuestion = () => {
   resetButtonStyles();
 
-  // Enable answer buttons for the new question
-  answerButtons.forEach(button => {
+  answerButtons.forEach((button) => {
     button.disabled = false;
   });
 
   if (questionNumber < questions.length) {
     let currentQuestion = questions[questionNumber];
 
-    // Display question text
     questionElement.innerText = currentQuestion.question;
 
-    // Assign each answer to a specific button
     answerButtons.forEach((button, i) => {
       button.innerText = currentQuestion.answers[i];
-      button.addEventListener('click', () => checkAnswer(answerButtons[i].innerText));
+      button.addEventListener("click", () =>
+        checkAnswer(answerButtons[i].innerText)
+      );
     });
 
-
-
-    // code adapted from https://github.com/jamesqquick/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript/blob/master/6.%20Create%20a%20Progress%20Bar/game.js
-
     progressText.innerText = `Question ${questionNumber + 1}/${MAX_QUESTIONS}`;
-    //Update the progress bar
+
     progressBarFull.style.width = `${(questionNumber / MAX_QUESTIONS) * 100}%`;
 
-
     questionNumber++;
-
-
   } else {
     gameOver();
   }
 };
 
 // Function to check if the selected answer is correct
+// Find button corresponding to the selected answer
+// Check if the selected answer is correct
+// Add 'correct' class for the correct answer to turn green
+
 const checkAnswer = (selectedAnswer) => {
   const currentQuestion = questions[questionNumber - 1];
 
-  // Find button corresponding to the selected answer
-  const selectedButton = answerButtonsArray.find(button => button.innerText === selectedAnswer);
+  const selectedButton = answerButtonsArray.find(
+    (button) => button.innerText === selectedAnswer
+  );
+  if (acceptingAnswers) {
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      incrementScore(CORRECT_BONUS);
+      console.log("Score after correct answer:", score);
 
-  // Check if the selected answer is correct
-  if (selectedAnswer === currentQuestion.correctAnswer) {
-    // Add 'correct' class for the correct answer to turn green
-    if (selectedButton) {
-      selectedButton.classList.add('correct');
-
+      if (selectedButton) {
+        selectedButton.classList.add("correct");
+      }
+    } else {
+      if (selectedButton) {
+        selectedButton.classList.add("wrong");
+      }
     }
-  } else {
-    // Add 'wrong' class for the incorrect answer to turn red
-    if (selectedButton) {
-      selectedButton.classList.add('wrong');
-    }
-
+    currentQuestion.answerChecked = true;
+    disableAnswerButtons();
+    acceptingAnswers = false;
   }
-  disableAnswerButtons();
-
-
 };
 
-
-
+//resets answer buttons for the next question
 
 function resetButtonStyles() {
   clearStatusClass(answerButtons);
-};
+}
 
+// clears the color and status of the buttons
 function clearStatusClass(element) {
-  element.forEach(btn => {
-    btn.classList.remove('correct', 'wrong');
+  element.forEach((btn) => {
+    btn.classList.remove("correct", "wrong");
   });
 }
 
+//prevents user from selecting multiple answers
+
 const disableAnswerButtons = () => {
-  const answerButtons = document.querySelectorAll('.btn-a');
-  answerButtons.forEach(button => {
+  const answerButtons = document.querySelectorAll(".btn-a");
+  answerButtons.forEach((button) => {
     button.disabled = true;
   });
 };
 
-document.querySelector('#next-btn').addEventListener('click', next);
+//NEXT button functionality which clears timer, displays new question and start timer again
 
+document.querySelector("#next-btn").addEventListener("click", next);
 
-function next(){
-      clearInterval(timeCounter);
-      getNewQuestion();
-      startTimer(20);
-
+function next() {
+  clearInterval(timeCounter);
+  getNewQuestion();
+  startTimer(20);
+  acceptingAnswers = true;
 }
 
 //function to activate level buttons
